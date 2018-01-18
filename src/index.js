102abd58441e6e98c1063a51cbf0b8625e2d4add
Kay Ehni <kay.ehnigmail.com>
2018-01-17 16:56:10 -0800

    webpack and file layout configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import _ from 'lodash';
+
+"use strict"
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
